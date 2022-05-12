784734bcd256517d17153adc0acfc3b19ffdde38
yishai <harelity@gmail.co.il>
2022-05-12 18:44:36 +0300

    play xo

---
+++ xo.js
@@ -0,0 +1,27 @@
+
+function initBoard(value){
+    for (i=0;i<value;i++)
+    for (j=0;j<value;j++)
+    {
+    list[i][j] = {
+        type: list[rand].typeOfCards,
+        color: list[rand].color,
+        key: list[rand].key,
+        isLife: list[rand].isLife,
+        player: list[rand].player,
+      };
+
+}
+function initPlayers(value){
+
+
+}
+function chekVictory(){
+
+
+}
+function changeTurn(){
+
+
+
+}
