313982de760c3e6d985e58a962eafb2f269f8d49
yishai <harelity@gmail.co.il>
2022-05-12 20:05:32 +0300

    ver2

--- xo.js
+++ xo.js
@@ -1,22 +1,33 @@
 
 function initBoard(value){
+  board=document.getElementById("board")
+  list=[];
     for (i=0;i<value;i++)
+    {
+    list[i]=[]
     for (j=0;j<value;j++)
     {
     list[i][j] = {
-        type: list[rand].typeOfCards,
-        color: list[rand].color,
-        key: list[rand].key,
-        isLife: list[rand].isLife,
-        player: list[rand].player,
+        type: 0,
+        key: "".concat(i,j)
+               
       };
+      console.log(list);
+      elem=document.createElement("div")
+      elem.id=list[i][j].key
+      elem.innerText="dsfsd"
+      elem.classList.add("styleE")
+      board.append(elem)
+
+    }
+  }
 
 }
-function initPlayers(value){
+function initPlayers(value){//itai elizur
 
 
 }
-function chekVictory(){
+function chekVictory(){//yishai
 
 
 }
@@ -25,3 +36,4 @@ function changeTurn(){
 
 
 }
+initBoard(3)
